36056fdc90295578480e07226a22639ac13c97f3
Cs23m011 <162670195+Cs23m011@users.noreply.github.com>
2024-03-17 22:19:55 +0530

    activation_function.py

--- cs6910_assignment/activation_function.py
+++ cs6910_assignment/activation_function.py
@@ -1,8 +1,10 @@
+# Import necessary modules
 import numpy as np
 import matplotlib.pyplot as plt
 import copy
+# Define a class for activation functions
 class ActivationFunctions:
-  def sigmoid(self,u):
+  def sigmoid(self,u):         # Sigmoid activation function
     x=[]
     for i in u:
       if i>200:
@@ -14,22 +16,22 @@ class ActivationFunctions:
     return np.array(x)
 
 
-  def sigmoidGrad(self,u):
+  def sigmoidGrad(self,u):           # Gradient of sigmoid activation function
     f=self.sigmoid(u)
     return f*(1-f)
 
-  def tanh(self,u):
+  def tanh(self,u):                  # Hyperbolic tangent (tanh) activation function
     return np.tanh(u)
 
-  def tanhGrad(self,u):
+  def tanhGrad(self,u):              # Gradient of tanh activation function
     return (1-np.square(np.tanh(u)))
 
-  def relu(self,u):
+  def relu(self,u):                  # Rectified Linear Unit (ReLU) activation function
     return np.maximum(0,u)
 
-  def reluGrad(self,u):
+  def reluGrad(self,u):              # Gradient of ReLU activation function
     return 1*(u>0)
 
-  def softmax(self,u):
+  def softmax(self,u):               # Softmax activation function
     x=np.exp(u-np.max(u))
     return x/np.sum(x)
